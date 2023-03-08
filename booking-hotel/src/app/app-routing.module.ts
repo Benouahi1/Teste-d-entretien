16efced4d30976a6c609dff8cf37ee5dccf9553d
ayoub benouahi <abenouahi5@gmail.com>
2023-03-08 10:37:55 +0100

    Cree page Login

--- booking-hotel/src/app/app-routing.module.ts
+++ booking-hotel/src/app/app-routing.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
-import { NavbarComponent } from './home/Nav/navbar/navbar.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 const routes: Routes = [
   {path: '', component: HomeComponent},
-  {path:'Nav', component: NavbarComponent}
+  {path:'Login', component: LoginComponent },
+  {path:'Register', component: RegisterComponent}
+
 ];
 
 @NgModule({
